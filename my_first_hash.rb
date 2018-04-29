886197eb201b3fd5b9d04cc8332d01fb51e593fc
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 20:57:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,8 +16,6 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
-end
-
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
