cacb204086c5d71db459354f88c890783af96912
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 20:53:26 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash {
+  "favorite artists" => "Young Thug, Vince Staples, Daft Punk",
+  "favorite book" => "LOTR"
+}
 end
 
 
